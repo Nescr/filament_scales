bf004e83168a942a671b3d2b9546d5e08b755ad7
Nescr <73983630+Nescr@users.noreply.github.com>
2024-07-26 11:45:16 +0300

    Add files via upload

---
+++ filament_scales/find_calibration_factor.ino
@@ -0,0 +1,27 @@
+#include "HX711.h"
+
+HX711 scale;
+
+#define DT  6
+#define SCK 5
+
+float weight_of_standard = 950;
+float units = 0.035274;
+float calibration_factor = 0;
+
+void setup()
+{
+  Serial.begin(9600);
+  scale.begin(DT, SCK);
+  scale.set_scale();
+  scale.tare();
+  Serial.println("You have 10 seconds to set your known load");
+  delay(10000);
+  Serial.print("calibration factor: ");
+  calibration_factor = scale.get_units(10) / (weight_of_standard / units);
+  Serial.println(calibration_factor);
+}
+
+void loop() 
+{
+}
---
+++ filament_scales/working_prototype.ino
@@ -0,0 +1,114 @@
+#include <EEPROM.h>
+#include "GyverEncoder.h"
+#include <Adafruit_GFX.h> 
+#include <Adafruit_SSD1306.h> 
+#include "HX711.h"
+
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1); 
+
+HX711 scale;
+
+#define DT  6
+#define SCK 5 
+
+#define S1 4
+#define S2 3
+#define KEY 2
+
+Encoder enc1(S1, S2, KEY);
+
+float calibration_factor = -10.57;  // СЮДА ВПИСАТЬ ЗНАЧЕНИЕ ИЗ ПЕРВОГО КОДА!!!!!!!!!!!
+float units;                      
+float ounces;                      
+
+int value;
+
+bool state = 0;
+
+unsigned long lastMillis = 0;
+unsigned long currentMillis = 0;
+
+void setup() 
+{
+  Serial.begin(9600);
+  EEPROM.get(0, value);
+  enc1.setType(TYPE2);
+  display_init();
+  delay(500);
+  scale.begin(DT, SCK);
+  scale.set_scale();
+  scale.tare();
+  scale.set_scale(calibration_factor);
+}
+
+void loop() 
+{
+  while (!enc1.isClick())
+  {
+    currentMillis = millis();
+    enc1.tick();
+    disp_print_weight();
+  }
+  while (!enc1.isClick())
+  {
+    enc1.tick();
+    currentMillis = millis();
+    disp_print_bad_weight();
+  }
+  EEPROM.put(0, value);
+}
+
+void display_init()
+{
+  Serial.begin(9600); 
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) 
+  {  
+    Serial.println(F("SSD1306 allocation failed")); 
+    for(;;); 
+  } 
+  display.display(); 
+  delay(1000);
+  display.clearDisplay(); 
+  display.setTextSize(3);
+  display.setTextColor(SSD1306_WHITE);
+}
+
+
+void disp_print_bad_weight()
+{
+  enc1.tick();
+  if (enc1.isRight()) 
+  {
+    value += 5;
+  }
+  if (enc1.isLeft()) 
+  {
+    value -= 5;
+  }
+  if (currentMillis - lastMillis >= 150) 
+  {
+    lastMillis = currentMillis;
+    display.clearDisplay(); 
+    display.setCursor(40, 20); 
+    display.print(value);  
+    display.display();
+  }
+}
+
+void disp_print_weight()
+{
+  if (currentMillis - lastMillis >= 200) 
+  {
+    lastMillis = currentMillis;
+    units = scale.get_units();
+    units = units / 10;
+    ounces = (units * 0.035274)*10;
+    display.clearDisplay(); 
+    display.setCursor(30, 25); 
+    display.print(int(ounces) - value);  
+    display.display();
+  }
+}
