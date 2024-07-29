1135dbd18cc1047af76dab09ad0484fb31d91a37
Nescr <73983630+Nescr@users.noreply.github.com>
2024-07-29 22:11:59 +0300

    find_calibration_factor.ino

--- filament_scales/find_calibration_factor.ino
+++ filament_scales/find_calibration_factor.ino
@@ -15,7 +15,7 @@ void setup()
   scale.begin(DT, SCK);
   scale.set_scale();
   scale.tare();
-  Serial.println("You have 10 seconds to set your known load");
+  Serial.println("у вас есть 10 секунд чтобы положить то, чего вес вы знаете");
   delay(10000);
   Serial.print("calibration factor: ");
   calibration_factor = scale.get_units(10) / (weight_of_standard / units);
